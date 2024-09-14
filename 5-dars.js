fce48bec91d550a6d599465205c3017651c2d12d
Rahmadjon <abdullayevrahmadjon821@gmail.com>
2024-09-14 12:04:22 +0500

    while do while for lop

---
+++ 5-dars.js
@@ -0,0 +1,47 @@
+// let i = 1;
+// let j = 1;
+// while (i < 11) {
+//     while (j < 10) {
+//         console.log(j + 'j');
+//         console.log(i + 'i');
+//         j++
+//         i++
+//     }
+// }
+
+// function
+// function getArt (){
+//     console.log('hellop js')
+//     console.log('hellop js')
+//     console.log('hellop js')
+//     console.log('================')
+// }
+
+// const getArt = function(name,age=20){
+//     console.log(`salom ${name}`)
+//     console.log(`yoshi ${age}`)
+// }
+// getArt('Rahmadjon',11)
+
+// const telegram = (name, surname='') => {
+// if (surname) console.log(name, surname)
+// else console.log(name);
+// console.log(name,surname||'');
+// console.log(name,surname);
+// }
+// telegram('Webrain',)
+// function return nima
+// const gpa = (a) => {
+//     let total = 193
+//     console.log(a * 100 / total
+//     return a * 100 / total
+// }
+// console.log(parseInt(gpa(159)))
+
+// document.body.innerHTML +=  "<div class='hello'><h1>salom dunyo</div>"
+// let parol = 'jjjiicddeij'
+// if (parol.length < 8) console.log('kamida 8 ta belgi kirirtilishi kerak');
+// else if (parol.length > 8) console.log('maxsimim 8 ta belgi');
+// else if (parol.length === 8) console.log('parol tasdiqlandi');
+
+// else true
