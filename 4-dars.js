07a06af9bb68dab30da2f50a5afafed624094118
Rahmadjon <abdullayevrahmadjon821@gmail.com>
2024-09-14 12:03:11 +0500

    if else switch case

---
+++ 4-dars.js
@@ -0,0 +1,40 @@
+// if -> <,> ,<=,>=,==,===  if shularni qabul qiladi
+// switch -> faqat ===,ni qabul qiladi shu bilan ifsan farqalanadi
+
+// let data = 'yomgir';
+// switch (data) {
+//     case 'yomgir':
+//         console.log('bugun yomgir');
+//         break
+//     case 'qor':
+//         console.log('bugun qor');
+//         break
+//     default:
+//         console.log('no data');
+
+// }
+
+// for lop
+
+let sum = 0
+// for (i = 0; i < 11; i++) {
+//     // sum += i
+//     if(sum===21){
+//         break
+//     }
+//     console.log(sum+=i)
+// }
+
+//     // console.log(`Sum = ${sum} + ${i} = ${sum += i}`)
+//     if(i%3===0) continue;
+//         console.log(i)
+// }
+// continue davom erttiradi break esa tuxtatadi
+
+for (i = 1; i < 10; i++) {
+    for (j = 1; j < 10; j++) {
+        console.log(i*j);
+        
+
+    }
+}
